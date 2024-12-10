3f4b29e103ed4b6020d843b9f7f7cd42f486b8b5
auto2028 <cen_3721@outlook.com>
2024-12-10 20:08:20 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -17,7 +17,7 @@ let urls = [];
 let subconverter = "SUBAPI.fxxk.dedyn.io"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
 let subProtocol = 'https';
-let PROXYIP = 'ts.hpc.tw'; // 新增的反向代理IP变量
+let PROXYIP = ''; // 新增的反向代理IP变量
 
 export default {
     async fetch (request,env) {
